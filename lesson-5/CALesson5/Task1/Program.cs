f32aa45ab665af77e6788ee515ec91f3194b1c9a
ArtemMS <savenkoam@gmail.com>
2021-03-21 22:41:31 +0900

    Complete lesson5 task1-4

---
+++ lesson-5/CALesson5/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {            
+            Console.Write("Enter your text to write to the file: ");
+            string textUser = Console.ReadLine();
+            WriteTextInFile(textUser); // вызов функции записи текста в файл
+        }
+
+        // функция записи текста в файл
+        public static void WriteTextInFile(string textUser)
+        {
+            Console.WriteLine(" ");
+            File.WriteAllText("newNote.txt", textUser);
+        }
+    }
+}
---
+++ lesson-5/CALesson5/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string path = Directory.GetCurrentDirectory();
+            string[] directory = path.Split('\\');            
+            string filename = "textPath.txt";           
+            string filenameR = "textPathR.txt";           
+            SavePathInFile(filename, directory); // вызов функции записи директории в файл
+            SavePathInFileRec(directory, filenameR); // вызов функции записи директории в файл с использованием рекурсии
+        }
+
+        // функция записи директории в файл 
+        public static void SavePathInFile(string file, string[] dir, string s = "|_")
+        {            
+            foreach (string item in dir)
+            {
+                File.AppendAllText(file, Environment.NewLine);
+                File.AppendAllText(file, $"{s} {item}");                
+                s ="  " + s;
+            }
+        }
+
+        // функция записи директории в файл реализованная с использованием рекурсии
+        public static void SavePathInFileRec(string[] dir, string file, string s = "|_", int i = 0)
+        {            
+            if (i < dir.Length)
+            {
+                File.AppendAllText(file, Environment.NewLine);
+                File.AppendAllText(file, $"{s} {dir[i]}");
+                s = "  " + s;
+                i++;                
+                SavePathInFileRec(dir, file, s, i);
+            }
+        }
+    }
+}
